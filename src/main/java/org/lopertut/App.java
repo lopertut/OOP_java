60b578b003325520d836b4c0c2f01fd27c0a02ef
Snus292 <georgiigiblov@gmail.com>
2024-09-16 14:30:24 +0300

    test

--- src/main/java/org/lopertut/App.java
+++ src/main/java/org/lopertut/App.java
@@ -1,5 +1,7 @@
 package org.lopertut;
 
+import java.lang.reflect.Array;
+
 public class App {
     public void run() {
         Address address = new Address();
@@ -10,16 +12,23 @@ public class App {
         address.setRoom("42");
         address.setZip("59028");
 
-        Person person = new Person("Artur", "Matvejev", 2006, 6, 13, address);
-        Employee employee = new Employee(person, "director", "3000");
-        System.out.println("name: " + employee.getPerson().getName());
-        System.out.println("surname: " + employee.getPerson().getSurname());
-        System.out.println("birthyear: " + employee.getPerson().getBirthday_year());
-        System.out.println("birthmonth: " + employee.getPerson().getBirthday_month());
-        System.out.println("birthday: " + employee.getPerson().getBirthday_day());
-        System.out.println("address: " + employee.getPerson().getAddress().getCity());
-        System.out.println("appointment: " + employee.getAppointment());
-        System.out.println("salary: " + employee.getSalary());
+        Person person1 = new Person("Artur", "Matvejev", 2006, 6, 13, address);
+        Person person2 = new Person("Maksim", "Korolev", 2007, 22, 9, address);
+        Employee employee1 = new Employee(person1, "director", "3000");
+        Employee employee2 = new Employee(person2, "employee", "1900");
 
+        Employee[] employees = {employee1, employee2};
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("name: " + employees[i].getPerson().getName());
+            System.out.println("surname: " + employees[i].getPerson().getSurname());
+            System.out.println("birthyear: " + employees[i].getPerson().getBirthday_year());
+            System.out.println("birthmonth: " + employees[i].getPerson().getBirthday_month());
+            System.out.println("birthday: " + employees[i].getPerson().getBirthday_day());
+            System.out.println("address: " + employees[i].getPerson().getAddress().getCity());
+            System.out.println("appointment: " + employees[i].getAppointment());
+            System.out.println("salary: " + employees[i].getSalary());
+            System.out.println("  ");
+        }
     }
 }
